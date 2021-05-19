fd78a0b4113717c6764447a9a80d8c32423d67ee
Thiago Ribeiro <thiagobcoelho25@gmail.com>
2021-05-19 15:36:18 -0300

    Aula 3

--- devpro/encurtador/views.py
+++ devpro/encurtador/views.py
@@ -7,3 +7,9 @@ from django.shortcuts import redirect, render
 def redirecionar(requisição, slug):
     url_redirect = UrlRedirect.objects.get(slug=slug)
     return redirect(url_redirect.destino)
+
+def relatorios(requisição, slug):
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    url_reduzida = requisição.build_absolute_uri(f'/{slug}')
+    contexto = {'reduce': url_redirect, 'url_reduzida' : url_reduzida}
+    return render(requisição, 'encurtador/relatorio.html', contexto)
